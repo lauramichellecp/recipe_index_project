5a9b9d571f3b79293f6a159d57f7918df7364352
Laura Calderon <calderon.l@northeastern.edu>
2022-04-19 20:51:33 -0400

    debugged procedures and functions

--- RecipeIndex_procedures.sql
+++ RecipeIndex_procedures.sql
@@ -2,12 +2,18 @@
 use recipe_index; 
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS getRecipeAuthor$$
+DROP FUNCTION IF EXISTS getRecipeAuthor$$
 
-CREATE PROCEDURE getRecipeAuthor(IN recipeId INT, OUT authorId INT) -- given a recipe id, returns the author id 
+CREATE FUNCTION getRecipeAuthor(recipeId INT) -- given a recipe id, returns the author id 
+RETURNS INT DETERMINISTIC
 BEGIN
-	SET authorId = (SELECT author FROM recipe WHERE rid = recipeId);
+	DECLARE authorID INT;
+    SET authorID = (SELECT author FROM recipe WHERE rid = recipeId);
+	RETURN authorID;
 END$$
+DELIMITER ;
+
+SELECT getRecipeAuthor(1);
 
 DELIMITER $$
 DROP FUNCTION IF EXISTS isRecipeAuthor$$
@@ -15,12 +21,12 @@ DROP FUNCTION IF EXISTS isRecipeAuthor$$
 CREATE FUNCTION isRecipeAuthor(recipeId INT, authorId INT) -- given a recipe id and an authorId returns true if author id 
 RETURNS BOOL DETERMINISTIC
 BEGIN
-	DECLARE author INT;
-    SET author = (SELECT author FROM recipe WHERE rid = recipeId);
-    
-	RETURN authorId = author; 
+	RETURN (SELECT EXISTS(SELECT author FROM recipe WHERE rid = recipeId AND author = authorId));
 END$$
+DELIMITER ;
 
+SELECT isRecipeAuthor(1, 2);
+SELECT isRecipeAuthor(1, 1);
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS getRecipesByAuthor$$
@@ -29,16 +35,21 @@ CREATE PROCEDURE getRecipesByAuthor(IN authorId INT) -- return all recipes given
 BEGIN
 	SELECT * FROM recipe WHERE author = authorId ORDER BY publish_date DESC;
 END$$
+DELIMITER ;
+
+CALL getRecipesByAuthor(2);
+CALL getRecipesByAuthor(1);
 
 DELIMITER $$ 
 DROP PROCEDURE IF EXISTS getRecipeByID $$
 
-CREATE PROCEDURE getRecipeByID(IN rid INT)
+CREATE PROCEDURE getRecipeByID(IN id INT)
 BEGIN 
-	SELECT * FROM recipe WHERE rid = rid;
+	SELECT * FROM recipe WHERE rid = id;
 END$$
+DELIMITER ;
 
-
+CALL getRecipeByID(1);
 
 -- Does this recipe follow this Dietary restriction?
 DELIMITER $$ 
@@ -51,7 +62,7 @@ BEGIN
     DECLARE ingredient INT;
 	DECLARE doesFollow BOOL DEFAULT TRUE;
 	DECLARE cur CURSOR FOR 
-		SELECT iid FROM recipe_ingredient WHERE rid = rid; -- returns all ingredients in the recipe
+		SELECT iid FROM recipe_ingredient WHERE rid = recipe_id; -- returns all ingredients in the recipe
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 	OPEN cur;
     
@@ -65,6 +76,13 @@ BEGIN
     CLOSE cur;
     RETURN doesFollow;
 END$$
+DELIMITER ;
+
+-- TODO: not working right now!
+SELECT recipeToDR(1, 1);
+SELECT recipeToDR(1, 2);
+SELECT recipeToDR(2, 1);
+SELECT recipeToDR(2, 2);
 
 DELIMITER $$ 
 DROP PROCEDURE IF EXISTS getRecipeByDietary $$
@@ -73,7 +91,7 @@ CREATE PROCEDURE getRecipeByDietary(IN drid INT) -- return all recipes given the
 BEGIN 
 	SELECT * FROM recipe WHERE recipeToDR(rid,drid);
 END$$
-
+DELIMITER ;
 
 -- return recipes of this course 
 DELIMITER $$
@@ -84,6 +102,10 @@ BEGIN
 	SELECT * FROM recipe WHERE course = recipe_course 
     ORDER BY publish_date DESC;
 END$$
+DELIMITER ;
+
+CALL getRecipesByCourse(1);
+CALL getRecipesByCourse(2);
 
 -- return recipes with a prep time less than the given
 DELIMITER $$
@@ -94,6 +116,10 @@ BEGIN
 	SELECT * FROM recipe WHERE prep_time <= prep_time_max
     ORDER BY publish_date DESC;
 END$$
+DELIMITER ;
+
+CALL getRecipesByPrepTime(100);
+CALL getRecipesByPrepTime(10);
 
 -- return recipes with a cook time less than the given
 DELIMITER $$
@@ -104,6 +130,7 @@ BEGIN
 	SELECT * FROM recipe WHERE cook_time <= cook_time_max
     ORDER BY publish_date DESC;
 END$$
+DELIMITER ;
 
 -- return recipes with a total time less than the given
 DELIMITER $$
@@ -114,30 +141,34 @@ BEGIN
 	SELECT * FROM recipe WHERE (cook_time + prep_time) <= time_max -- idk if this works
     ORDER BY publish_date DESC;
 END$$
+DELIMITER ;
 
 -- return all bookmarks for a user by uid
 DELIMITER $$
 DROP PROCEDURE IF EXISTS getBookmarksByUser$$
 
-CREATE PROCEDURE getBookmarksByUser(IN uid INT) 
+CREATE PROCEDURE getBookmarksByUser(IN id INT) 
 BEGIN
-	SELECT * FROM user WHERE uid = uid; 
+	SELECT * FROM bookmark WHERE uid = id; 
 END$$
+DELIMITER ;
 
 -- create a recipe
 -- add to python code - add dietary restriction to all ing
 -- we couldnt do a function returning sql_msg 
+
+DROP PROCEDURE IF EXISTS createRecipe$$
 DELIMITER $$
 CREATE PROCEDURE createRecipe(r_name VARCHAR(64),
-								prep_time INT,
-								cook_time INT,
-								serving_size INT,
-								cuisine VARCHAR(16),
-								instruct VARCHAR(1000),
-								note VARCHAR(200),
-								descrip VARCHAR(500),
-                                author INT,
-								recipe_course VARCHAR(16))
+								r_prep_time INT,
+								r_cook_time INT,
+								r_serving_size INT,
+								r_cuisine VARCHAR(16),
+								r_instruct VARCHAR(1000),
+								r_note VARCHAR(200),
+								r_descrip VARCHAR(500),
+                                r_author INT,
+								r_course VARCHAR(16))
 BEGIN 
 	DECLARE course_id INT;
     DECLARE sql_msg VARCHAR(16);
@@ -147,24 +178,24 @@ BEGIN
     START TRANSACTION;
     
     IF 
-      (SELECT EXISTS(SELECT cid FROM course WHERE course_name = recipe_course)) -- course exists
+      (SELECT EXISTS(SELECT cid FROM course WHERE course_name = r_course)) -- course exists
     THEN 
-      SET course_id = (SELECT cid FROM course WHERE course_name = recipe_course);
+      SET course_id = (SELECT cid FROM course WHERE course_name = r_course);
 	ELSE
-      INSERT INTO course (course_name) VALUES (recipe_course);
-      SET course_id = (SELECT cid FROM course WHERE course_name = recipe_course);
+      INSERT INTO course (course_name) VALUES (r_course);
+      SET course_id = (SELECT cid FROM course WHERE course_name = r_course);
 	END IF;
-      
+    
     IF 
-	  (SELECT EXISTS(SELECT recipe_name FROM recipe WHERE recipe_name = rname)) 
+	  (SELECT EXISTS(SELECT recipe_name FROM recipe WHERE recipe_name = r_name))
 	THEN 
 		SET sql_error = TRUE;
 --        SET @sql_msg = "Failed on recipe name";
 	ELSE 
-      INSERT INTO recipe (recipe_name, prep_time, cook_time, serving_size, 
-    cuisine, instructions, notes, description, author, course) VALUES
-	(r_name, prep_time, cook_time, serving_size,
-    cuisine, instruct, note, descrip, author, course_id);
+		INSERT INTO recipe (recipe_name, prep_time, cook_time, serving_size, 
+		cuisine, instructions, notes, description, author, course) VALUES
+		(r_name, r_prep_time, r_cook_time, r_serving_size,
+		r_cuisine, r_instruct, r_note, r_descrip, r_author, course_id);
 	END IF;
 
 	IF sql_error = FALSE THEN
@@ -174,6 +205,22 @@ BEGIN
 	END IF;
 END $$
 DELIMITER ;
+
+CALL createRecipe('No-Bake Chocolate Strawberry Coconut Bars', 20, 10, 8, 'American',
+				'Line a 8x8 pan with a parchment paper with extra parchment hanging over the sides.',
+				'something',
+				'This delicious triple-layered chocolate strawberry coconut bar is no-bake, only uses 6 ingredients',
+				1,
+				'Breakfast');
+
+SELECT * FROM recipe;
+
+CALL addIngredients(1, 'salt and pepper', 1, 'a pinch');
+CALL addIngredients(1, 'salt and pepper', 2, 'a pinch');
+
+SELECT * FROM ingredient;
+SELECT * FROM ingredient_follows;
+SELECT * FROM recipe_ingredient;
 
 DELIMITER $$
 CREATE PROCEDURE addIngredients(recipe_id INT, 
@@ -207,6 +254,7 @@ BEGIN
         VALUES (recipe_id, ing_id, amt);
 	END IF;
 END$$
+DELIMITER ;
 
 -- create a user 
 
@@ -223,6 +271,11 @@ BEGIN
 		(user_first_name, user_last_name, user_email, user_password);
 	END IF;
 END$$
+DELIMITER ;
+
+CALL createUser('first', 'last', 'email@something.com', 'pass');
+
+SELECT * FROM user;
 
 -- create bookmark for a user 
 
@@ -235,6 +288,11 @@ BEGIN
       INSERT INTO bookmark (uid, recipe) VALUES (user_id, recipe_id);
 	END IF;
 END $$
+DELIMITER ;
+
+CALL createBookmark(1, 2);
+CALL getBookmarksByUser(2);
+CALL getBookmarksByUser(1);
 
 -- get recipe by name 
 
@@ -243,6 +301,7 @@ CREATE PROCEDURE getRecipeByName(rname VARCHAR(64))
 BEGIN
 	SELECT * FROM recipe WHERE recipe_name = rname;
 END$$
+DELIMITER ;
 
 
 -- trigger to delete the ingredient to recipe links
@@ -288,11 +347,14 @@ FILTER ON
 
 UPDATE 
 - authors can update their own recipes -- isRecipeAuthor can be used to test
-- all users can update their own bookmarks 
+- all users can update their own bookmarks
 
 DELETE 
 - authors can delete their own recipes
 - all users can delete their own bookmarks
+
+
+Put all these in a sql_utils.py file.
 **/
 
 /**
